a210e66066542a59744e4ca8a51e13b3bb37cd35
Priyanka Mahale <97458322+Primahale@users.noreply.github.com>
2024-07-26 21:25:42 +0530

    Update articalList.js

--- src/components/articalList.js
+++ src/components/articalList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+const urlRegex = /^(https?:\/\/)?([\w\d-]+\.)+[a-z]{2,6}(\/[\w\d-]*)*\/?$/;
 
 const ArticleList = ({ articles }) => {
-  
+  const validatedArticles = articles.map(article => ({
+    ...article,
+    url: urlRegex.test(article.url) ? article.url : '#',
   // console.log(articles)
   return (
     <ul>
-      {articles.map((article, index) => (
+      {validatedArticles.map((article, index) => (
         <li key={index}>
           <a href={article.url} target="_blank" rel="noopener noreferrer">
             <h2>{article.title}</h2>
