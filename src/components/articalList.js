baa87ac2749bcbf6f02bafc2fe616d8c5764e265
Priyanka Mahale <97458322+Primahale@users.noreply.github.com>
2024-07-26 21:43:56 +0530

    regex date added

--- src/components/articalList.js
+++ src/components/articalList.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
 const ArticleList = ({ articles }) => {
+  const processedArticles = articles.map(article => ({
+    ...article,
+    date: dateRegex.test(article.date) ? article.date : 'Invalid date',
+  }));
   
   // console.log(articles)
   return (
     <ul>
-      {articles.map((article, index) => (
+      {processedArticles.map((article, index) => (
         <li key={index}>
           <a href={article.url} target="_blank" rel="noopener noreferrer">
             <h2>{article.title}</h2>
             <p>Author : {article.author}</p>
+            <p>Date: {article.date}</p>
           </a>
         </li>
       ))}
