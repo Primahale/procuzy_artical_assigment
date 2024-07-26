e9c36b95e41a78ea5d634f1a037e602cbe57bf1f
Priyanka Mahale <97458322+Primahale@users.noreply.github.com>
2024-07-26 21:52:48 +0530

    Update articalList.js

--- src/components/articalList.js
+++ src/components/articalList.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 
 const ArticleList = ({ articles }) => {
-
+  
   // console.log(articles)
   return (
     <ul>
-      {articals.map((article, index) => (
+      {articles.map((article, index) => (
         <li key={index}>
           <a href={article.url} target="_blank" rel="noopener noreferrer">
             <h2>{article.title}</h2>
             <p>Author : {article.author}</p>
-            // <p>Date: {article.date}</p>
           </a>
         </li>
       ))}
